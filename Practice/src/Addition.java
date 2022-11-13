d2395eca9ee721bb288c88e7331dfb93f897c2be
Acer <Acer@LAPTOP-VEGIS00O>
2022-11-14 00:33:37 +0530

    just changed the addition values

--- Practice/src/Addition.java
+++ Practice/src/Addition.java
@@ -1,6 +1,6 @@
 
 public class Addition {
 	public static void main(String[] args) {
-		System.out.println("addition 98 and 56 : "+(98+56));
+		System.out.println("addition 98 and 56 : "+(100+2000));
 	}
 }
